47972859bf2e8a00f1113246f0936695785acbb3
VegetaGoat <185959786+VegetaGoat@users.noreply.github.com>
2025-02-18 22:50:28 +0100

    Stworzenie tabeli w html i dodanie stylow w css. W js, stworzenie tablicy ktora przechowuje obiekty, dodanie obiektow, stworzenie metody addBookToLibrary, która dodaje obiekt do tablicy myLibrary. Storzenie metody showBooks, która wypisuje wszystkie obiekty i tworzy komórki tabeli w których znajduje się index(th) i obiekt(td)

--- script.js
+++ script.js
@@ -1,15 +1,44 @@
-function Book(title, author, pages, IsRead){
+const myLibrary = [];
+
+
+function Book(title, author, pages, isRead){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.IsRead = IsRead;
+    this.isRead = isRead;
 
     this.info = function(){
-        return(`${this.title} by ${this.author}, ${this.pages} pages, ${this.IsRead}`);
+        return(`${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`);
     }
 
 }
 
-const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+function addBookToLibrary(title, author, pages, isRead){
+    const Books = new Book(title, author, pages, isRead);
+    myLibrary.push(Books);
+}
 
-console.log(theHobbit.info());
+function showBooks(){
+    const table = document.querySelector("#libraryTable tbody")
+    for(i = 0; i < myLibrary.length; i++){
+        const row = document.createElement("tr");
+        
+        const indexCell = document.createElement("th");
+        indexCell.textContent = i + 1;
+        row.appendChild(indexCell);
+
+        const bookCell = document.createElement("td");
+        bookCell.textContent = `${myLibrary[i].title} by ${myLibrary[i].author}, ${myLibrary[i].pages} pages, ${myLibrary[i].isRead}`;
+        row.appendChild(bookCell);
+
+        table.appendChild(row);
+    }
+}
+
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
+addBookToLibrary("1984", "Gorge Orwell", 350, "read");
+addBookToLibrary("The Republic", "Plato", 500, "not read yet");
+addBookToLibrary("Pan Tadeusz", "Adam Mickiewicz", 400, "read");
+
+showBooks()
